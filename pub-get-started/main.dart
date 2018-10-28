4b56d41d57f213e4546c5f1663d5fa0e78025cb7
jacekk <github.com@jacek-k.net>
2018-10-28 11:13:40 +0100

    moved some comments within one file;

--- pub-get-started/main.dart
+++ pub-get-started/main.dart
@@ -1,5 +1,13 @@
 import 'package:args/args.dart';
 
+// $ dart pub-get-started/main.dart -m test -n 66 -- more --flags here
+// Options: {mode, line-number, verbose}
+// Arguments: [-m, test, -n, 66, --, more, --flags, here]
+// Rest: [more, --flags, here]
+// Verbose: false
+// Mode: test
+// Line number: 66
+
 void main(List<String> arguments) {
   final parser = new ArgParser()
     ..addOption('line-number', abbr: 'n', defaultsTo: '10')
@@ -16,11 +24,3 @@ void main(List<String> arguments) {
   print('Mode: ${result["mode"]}');
   print('Line number: ${result["line-number"]}');
 }
-
-// $ dart pub-get-started/main.dart -m test -n 66 -- more --flags here
-// Options: {mode, line-number, verbose}
-// Arguments: [-m, test, -n, 66, --, more, --flags, here]
-// Rest: [more, --flags, here]
-// Verbose: false
-// Mode: test
-// Line number: 66
